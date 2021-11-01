dc661d2638747b37ad5b57342e0212537ac89e86
OtKsue <90846089+OtKsue@users.noreply.github.com>
2021-11-01 15:29:38 +0300

    Update Tricorn.java

--- lab5/Tricorn.java
+++ lab5/Tricorn.java
@@ -1,7 +1,6 @@
 import java.awt.geom.Rectangle2D;
 
-public class Tricorn extends FractalGenerator{
-
+public class Tricorn extends FractalGenerator {
     public static final int MAX_ITERATIONS = 2000;
     @Override
     public void getInitialRange(Rectangle2D.Double range) {
@@ -15,7 +14,7 @@ public class Tricorn extends FractalGenerator{
     public int numIterations(double x0, double y0) {
         int i = 0;
         double x = 0, y = 0;
-        while (x * x + y * y <= 2*2 && i < MAX_ITERATIONS){
+        while (x * x + y * y <= 2*2 && i < MAX_ITERATIONS) {
             y *= -1;
             double temp = x * x - y * y + x0;
             y = 2 * x * y + y0;
